8bd4240509443b7a484f0304f0f6636094900e0a
koshchey <sofiakh98@gmail.com>
2025-07-13 13:56:49 +0800

    depth launch

---
+++ pioneer_ws/src/launch_bringup/launch/stereo_launch.py
@@ -0,0 +1,116 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription, launch_description_sources
+from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+import launch_ros.actions
+import launch_ros.descriptions
+
+# Adapted from stereo.launch.py from the depthai repo
+
+def generate_launch_description():
+    default_rviz = os.path.join(get_package_share_directory('depthai_examples'),
+                                'rviz', 'stereoPointCloud.rviz')
+    urdf_launch_dir = os.path.join(get_package_share_directory('depthai_descriptions'), 'launch')
+    
+    camera_model = LaunchConfiguration('camera_model',  default = 'OAK-D')
+    tf_prefix    = LaunchConfiguration('tf_prefix',   default = 'oak')
+    base_frame   = LaunchConfiguration('base_frame',    default = 'oak-d_frame')
+    parent_frame = LaunchConfiguration('parent_frame',  default = 'oak-d-base-frame')
+
+    cam_pos_x  = LaunchConfiguration('cam_pos_x',     default = '0.0')
+    cam_pos_y  = LaunchConfiguration('cam_pos_y',     default = '0.0')
+    cam_pos_z  = LaunchConfiguration('cam_pos_z',     default = '0.0')
+    cam_roll   = LaunchConfiguration('cam_roll',      default = '0.0')
+    cam_pitch  = LaunchConfiguration('cam_pitch',     default = '0.0')
+    cam_yaw    = LaunchConfiguration('cam_yaw',       default = '0.0')
+
+    mode           = LaunchConfiguration('mode', default = 'depth')
+    lrcheck        = LaunchConfiguration('lrcheck', default = True)
+    extended       = LaunchConfiguration('extended', default = False)
+    subpixel       = LaunchConfiguration('subpixel', default = True)
+    confidence     = LaunchConfiguration('confidence', default = 200)
+    LRchecktresh   = LaunchConfiguration('LRchecktresh', default = 5)
+    monoResolution = LaunchConfiguration('monoResolution',  default = '720p')
+
+    # removed DeclareLaunchArgument since we aren't passing from command line
+
+    urdf_launch = IncludeLaunchDescription(
+                            launch_description_sources.PythonLaunchDescriptionSource(
+                                    os.path.join(urdf_launch_dir, 'urdf_launch.py')),
+                            launch_arguments={'tf_prefix' : tf_prefix,
+                                              'camera_model': camera_model,
+                                              'base_frame'  : base_frame,
+                                              'parent_frame': parent_frame,
+                                              'cam_pos_x'   : cam_pos_x,
+                                              'cam_pos_y'   : cam_pos_y,
+                                              'cam_pos_z'   : cam_pos_z,
+                                              'cam_roll'    : cam_roll,
+                                              'cam_pitch'   : cam_pitch,
+                                              'cam_yaw'     : cam_yaw}.items())
+
+    stereo_node = launch_ros.actions.Node(
+            package='depthai_examples', executable='stereo_node',
+            output='screen',
+            parameters=[{'tf_prefix': tf_prefix},
+                        {'mode': mode},
+                        {'lrcheck': lrcheck},
+                        {'extended': extended},
+                        {'subpixel': subpixel},
+                        {'confidence': confidence},
+                        {'LRchecktresh': LRchecktresh},
+                        {'monoResolution': monoResolution}])
+
+
+    metric_converter_node = launch_ros.actions.ComposableNodeContainer(
+            name='container',
+            namespace='',
+            package='rclcpp_components',
+            executable='component_container',
+            composable_node_descriptions=[
+                # Driver itself
+                launch_ros.descriptions.ComposableNode(
+                    package='depth_image_proc',
+                    plugin='depth_image_proc::ConvertMetricNode',
+                    name='convert_metric_node',
+                    remappings=[('image_raw', '/stereo/depth'),
+                                ('camera_info', '/stereo/camera_info'),
+                                ('image', '/stereo/converted_depth')]
+                ),
+            ],
+            output='screen',)
+
+    point_cloud_node = launch_ros.actions.ComposableNodeContainer(
+            name='container2',
+            namespace='',
+            package='rclcpp_components',
+            executable='component_container',
+            composable_node_descriptions=[
+                # Driver itself
+                launch_ros.descriptions.ComposableNode(
+                    package='depth_image_proc',
+                    plugin='depth_image_proc::PointCloudXyziNode',
+                    name='point_cloud_xyzi',
+
+                    remappings=[('depth/image_rect', '/stereo/converted_depth'),
+                                ('intensity/image_rect', '/right/image_rect'),
+                                ('intensity/camera_info', '/right/camera_info'),
+                                ('points', '/stereo/points')]
+                ),
+            ],
+            output='screen',)
+
+    rviz_node = launch_ros.actions.Node(
+            package='rviz2', executable='rviz2', output='screen',
+            arguments=['--display-config', default_rviz])
+
+    ld = LaunchDescription()
+
+    ld.add_action(stereo_node)
+    ld.add_action(urdf_launch) # tells you where the camera is in rviz 
+    ld.add_action(metric_converter_node) # this converts data from the camera to physical units
+    ld.add_action(point_cloud_node)
+    ld.add_action(rviz_node)
+    return ld
---
+++ pioneer_ws/src/launch_bringup/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'launch_bringup'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        # Launch files:
+        ('share/' + package_name + '/launch', ['launch/stereo_launch.py']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='pioneer1',
+    maintainer_email='pioneer1@todo.todo',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
